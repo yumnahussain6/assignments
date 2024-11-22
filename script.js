ba9afc602e198a1c6e368ee3ffd7dcf85fd5c973
yumnahussain6 <yumna.hosain@gmail.com>
2024-11-23 00:06:37 +0500

    work

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-document.write(a123)
+let firstname = "Yumna";
+let lastname = "Hussain";
+let qualification = "Intermediate";
+let age = '18';
+let fathername = "Salman Hussain";
+let city = "Karachi";
+let mothername = "Shazia Salman";
+
+document.write("<h1 style='color:palevioletred'> My first name is: " + firstname + "</h1>");
+document.write("<h1 style='color:palevioletred'> My last name is: " + lastname + "</h1>");
+document.write("<h2 style='color:palevioletred'> My mother's name is: " + mothername + "</h2>");
+document.write("<h2 style='color:palevioletred'> My father's name is: " + fathername + "</h2>");
+document.write("<h4 style='color:palevioletred'> My qualification is: " + qualification + "</h4>");
+document.write("<h4 style='color:palevioletred'> My age is: " + age + "</h4>");
+document.write("<h4 style='color:palevioletred'> I live in: " + city + "</h4>");
