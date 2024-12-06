55f578c7eb056b9ea88137c608b643b3697d79ed
yumnahussain6 <yumna.hosain@gmail.com>
2024-12-07 01:06:16 +0500

    a

--- assignemnts/script.js
+++ assignemnts/script.js
@@ -1,11 +1,11 @@
 let firstname = prompt("Enter your name: ")
-let lastname = prompt("Enter your last name: ");
-let qualification = prompt("Enter your qualification: ");
-let age = prompt("Enter your age: ");
-let fathername = prompt("Enter your father's name: ");
-let city = prompt("Enter your city: ");
-let mothername = prompt("Enter your mother's name: ");
-prompt("Your information has been submitted!");
+let lastname = prompt("Enter your last name: ")
+let qualification = prompt("Enter your qualification: ")
+let age = prompt("Enter your age: ")
+let fathername = prompt("Enter your father's name: ")
+let city = prompt("Enter your city: ")
+let mothername = prompt("Enter your mother's name: ")
+prompt("Your information has been submitted!")
 
 document.write("<h1 style='color:palevioletred'> My first name is: " + firstname + "</h1>");
 document.write("<h1 style='color:palevioletred'> My last name is: " + lastname + "</h1>");
